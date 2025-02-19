4f179240dc60c22fd736444a99ab81bca60cf09b
Mehmethan YUMUS <mehmethanyumus@Mehmethans-MacBook-Air.local>
2025-02-19 09:53:27 +0000

    Kitap Model sınıfı oluşturuldu.

---
+++ lib/model/kitap_model.dart
@@ -0,0 +1,24 @@
+class KitapModel {
+
+  int? kitap_id;
+  String kitap_ad;
+  DateTime kitap_cdate;
+  DateTime kitap_udate;
+
+  KitapModel(this.kitap_ad, this.kitap_cdate, this.kitap_udate);
+
+  KitapModel.fromMap(Map<String, dynamic> map):
+      kitap_ad = map["kitap_ad"],
+      kitap_cdate = DateTime.fromMillisecondsSinceEpoch(map["kitap_cdate"]),
+      kitap_udate = DateTime.fromMillisecondsSinceEpoch(map["kitap_udate"]);
+
+  Map<String, dynamic> toMap(){
+    return {
+      "kitap_id": kitap_id,
+      "kitap_ad": kitap_ad,
+      "kitap_cdate": kitap_cdate,
+      "kitap_udate": kitap_udate
+    };
+  }
+
+}
