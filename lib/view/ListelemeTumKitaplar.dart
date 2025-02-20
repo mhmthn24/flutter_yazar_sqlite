00ece4b3cbd50aa7bcb7052a53983574d30c4220
Mehmethan YUMUS <mehmethanyumus@Mehmethans-MacBook-Air.local>
2025-02-20 02:41:03 +0000

    Listeleme ekranına Alert Dialog eklendi.

--- lib/view/ListelemeTumKitaplar.dart
+++ lib/view/ListelemeTumKitaplar.dart
@@ -7,7 +7,7 @@ class Listelemetumkitaplar extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: _build_appBar(),
-      floatingActionButton: _build_fab(),
+      floatingActionButton: _build_fab(context),
       //body: _build_body(),
     );
   }
@@ -18,12 +18,47 @@ class Listelemetumkitaplar extends StatelessWidget {
     );
   }
 
-  Widget _build_fab(){
+  Widget _build_fab(BuildContext context){
     return FloatingActionButton(
-      onPressed: (){},
+      onPressed: (){
+        _build_alert_dialog(context);
+      },
       child: Icon(Icons.add),
     );
   }
 
+  Future<String?> _build_alert_dialog(BuildContext context){
+    return showDialog<String>(
+      context: context,
+      builder: (context){
+
+        String? kitapAdi;
+
+        return AlertDialog(
+          title: Text("Kitap Ekle"),
+          content: TextField(
+            onChanged: (String kullaniciGiris){
+              kitapAdi = kullaniciGiris;
+            },
+          ),
+          actions: [
+            Row(
+              mainAxisSize: MainAxisSize.max,
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                TextButton(
+                    onPressed: (){
+                      Navigator.pop(context);
+                    },
+                    child: Text("Vazgeç")
+                ),
+                TextButton(onPressed: (){}, child: Text("Ekle"))
+              ],
+            )
+          ],
+        );
+      },
+    );
+  }
 
 }
