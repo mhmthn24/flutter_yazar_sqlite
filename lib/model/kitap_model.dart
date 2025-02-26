ecc1f570c115db6fd158a7955104191359e1807e
Mehmethan YUMUS <mehmethanyumus@Mehmethans-MacBook-Air.local>
2025-02-26 22:44:57 +0000

    Kitap Model sınıfına kategori bilgisi eklendi ve Sabitler sınıfı oluşturuldu.

--- lib/model/kitap_model.dart
+++ lib/model/kitap_model.dart
@@ -4,14 +4,21 @@ class KitapModel {
   String kitap_ad;
   DateTime kitap_cdate;
   DateTime kitap_udate;
+  int kitap_kategori;
 
-  KitapModel(this.kitap_ad, this.kitap_cdate, this.kitap_udate);
+  KitapModel(
+      this.kitap_ad,
+      this.kitap_cdate,
+      this.kitap_udate,
+      this.kitap_kategori,
+  );
 
   KitapModel.fromMap(Map<String, dynamic> map):
       kitap_id = map["kitap_id"],
       kitap_ad = map["kitap_ad"],
       kitap_cdate = DateTime.fromMillisecondsSinceEpoch(map["kitap_cdate"]),
-      kitap_udate = DateTime.fromMillisecondsSinceEpoch(map["kitap_udate"]);
+      kitap_udate = DateTime.fromMillisecondsSinceEpoch(map["kitap_udate"]),
+      kitap_kategori = map["kitap_kategori"];
 
   Map<String, dynamic> toMap(){
     return {
@@ -19,6 +26,7 @@ class KitapModel {
       "kitap_ad": kitap_ad,
       "kitap_cdate": kitap_cdate.millisecondsSinceEpoch,
       "kitap_udate": kitap_udate.millisecondsSinceEpoch,
+      "kitap_kategori": kitap_kategori
     };
   }
 
