1f312bd4aad972b5899b073042c8860b0ce0671b
Mehmethan YUMUS <mehmethanyumus@Mehmethans-MacBook-Air.local>
2025-03-14 02:31:45 +0000

    Main.dart dosyasında setupLocator fonksiyonu çağırıldı.

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_yazar_sqlite/tools/locator.dart';
 import 'package:flutter_yazar_sqlite/view/listeleme_kitaplar.dart';
-import 'package:flutter_yazar_sqlite/view_model/listeleme_bolumler_view_model.dart';
 import 'package:flutter_yazar_sqlite/view_model/listeleme_kitaplar_view_model.dart';
 import 'package:provider/provider.dart';
 
 void main() {
+  setupLocator();
   runApp(AnaUygulama());
 }
 
